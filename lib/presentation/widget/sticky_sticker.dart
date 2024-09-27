c78c1224c8777d2d3c3b627e81eb2a0cedf5915d
Anik <sheikhanikbd@gmail.com>
2024-09-27 12:17:23 +0600

    🔍 zoom added

--- lib/presentation/widget/sticky_sticker.dart
+++ lib/presentation/widget/sticky_sticker.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:pin_bord/models/sticky/sticky.dart';
+
+import '../../provider/note_color_provider.dart';
+import '../../provider/sticky_provider.dart';
 // import 'package:flutter_svg/flutter_svg.dart';
 
 class StickySticker extends ConsumerWidget {
@@ -19,27 +23,33 @@ class StickySticker extends ConsumerWidget {
       child: Stack(
         children: [
           Container(
-            constraints: const BoxConstraints(minHeight: 100),
+            // constraints: const BoxConstraints(minHeight: 100),
             padding: const EdgeInsets.all(10),
             // color: sticky.color ?? ref.read(colorsProvider).last,
-            // child: SvgPicture.string(sticky.content),
+            child: SvgPicture.string(sticky.content),
           ),
-          // Positioned(
-          //   top: 0,
-          //   right: 0,
-          //   child: Container(
-          //     padding: const EdgeInsets.all(2),
-          //     decoration: BoxDecoration(
-          //       borderRadius: BorderRadius.circular(8),
-          //       color: Colors.grey.shade500,
-          //     ),
-          //     child: const Icon(
-          //       Icons.close,
-          //       size: 12,
-          //       color: Colors.white,
-          //     ),
-          //   ),
-          // )
+          Positioned(
+            top: 0,
+            right: 0,
+            child: GestureDetector(
+              onTap: () {
+                final notifier = ref.read(stickyProvider.notifier);
+                notifier.removeSticky(sticky.id);
+              },
+              child: Container(
+                padding: const EdgeInsets.all(2),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.grey.shade500,
+                ),
+                child: const Icon(
+                  Icons.close,
+                  size: 12,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
